1b1d07c3362465701488f3d090dfdd455705ac13
Dafi Majid <dafi.majid@gmail.com>
2024-12-10 00:49:13 +0700

    adjust env variable

--- app/database.go
+++ app/database.go
@@ -10,12 +10,13 @@ import (
 )
 
 func NewDb() *gorm.DB {
-	port := os.Getenv("PGPORT")
-	user := os.Getenv("PGUSER")
-	password := os.Getenv("PGPASSWORD")
-	schema := os.Getenv("PGSCHEMA")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	schema := os.Getenv("POSTGRES_SCHEMA")
+	host := os.Getenv("POSTGRES_HOST")
 
-	dsn := "host=localhost user=" + user + " password=" + password + " dbname=" + schema + " port=" + port + " sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + schema + " port=" + port + " sslmode=disable TimeZone=Asia/Jakarta"
 	// dsn := user + ":" + password + "@tcp(localhost:" + port + ")/" + schema + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
